70f9e40a324083faa00ccb8ccb4dee910c70a499
m <mustafagokce009@gmail.com>
2022-04-29 13:32:10 +0300

    change alt frames to absolute

--- scripts/plane_follow.lua
+++ scripts/plane_follow.lua
@@ -8,31 +8,26 @@ local target_velocity = Vector3f()
 local target_heading = 0.0
 local have_target = false
 
--- bind a parameter to a variable
-function bind_param(name)
+function bind_param(name) -- bind a parameter to a variable
     local p = Parameter()
     assert(p:init(name), string.format("could not find %s parameter", name))
     return p
 end
 
--- add a parameter and bind it to a variable
-function bind_add_param(name, idx, default_value)
+function bind_add_param(name, idx, default_value) -- add a parameter and bind it to a variable
     assert(param:add_param(PARAM_TABLE_KEY, idx, name, default_value), string.format("could not add param %s", name))
     return bind_param(PARAM_TABLE_PREFIX .. name)
 end
 
--- check key parameters
-function check_parameters()
+function check_parameters() -- check key parameters
 
-    -- parameters to check
     local key_params = {
         FOLL_ENABLE = 1,
         FOLL_OFS_TYPE = 1,
         FOLL_ALT_TYPE = 0,
-    }
+    } -- parameters to check
 
-    -- check and set parameters
-    for p, v in pairs(key_params) do
+    for p, v in pairs(key_params) do -- check and set parameters
         local current = param:get(p)
         assert(current, string.format("parameter %s not found", p))
         if math.abs(v - current) > 0.001 then
@@ -42,26 +37,25 @@ function check_parameters()
     end
 end
 
--- update target state
-function update_target()
+function update_target() -- update target state
     if not follow:have_target() then
         if have_target then
             gcs:send_text(0, "lost leader")
         end
-        have_target = false
+        have_target = false -- vehicle does not have a leader
         return
     end
     if not have_target then
         gcs:send_text(0, "found leader")
     end
-    have_target = true
+    have_target = true -- vehicle has a leader
 
-    target_pos, target_velocity = follow:get_target_location_and_velocity_ofs()
-    target_heading = follow:get_target_heading_deg()
+    target_pos, target_velocity = follow:get_target_location_and_velocity_ofs() -- get location and velocity of the leader
+    target_pos:change_alt_frame(0) -- altitude frame type of the target position should be absolute
+    target_heading = follow:get_target_heading_deg() -- -- get heading of the leader
 end
 
--- main function that will be called within loop
-function update()
+function update() -- main function that will be called within loop
     update_target() -- update target data
     if not have_target then
         return -- do not proceed if vehicle does not have a target yet
@@ -79,17 +73,17 @@ function update()
         return -- do not proceed if vehicle is not in a flight mode with target location
     end
 
+    next_WP:change_alt_frame(0) -- change altitude frame of the next position to absolute
+
     vehicle:update_target_location(next_WP, target_pos) -- update target location of the vehicle
 end
 
--- loop function to call main update function
-function loop()
+function loop() -- loop function to call main update function
     update()
     return loop, 1000 // LOOP_UPDATE_RATE_HZ
 end
 
--- protected loop function to call main update function
-function protected_wrapper()
+function protected_wrapper() -- protected loop function to call main update function
     local success, err = pcall(update)
     if not success then
         gcs:send_text(0, "Internal Error: " .. err)
@@ -98,8 +92,6 @@ function protected_wrapper()
     return protected_wrapper, 1000 // LOOP_UPDATE_RATE_HZ
 end
 
--- check parameters before starting the main loop
-check_parameters()
+check_parameters() -- check parameters before starting the main loop
 
--- initial call for protected loop function
-return protected_wrapper()
+return protected_wrapper() -- initial call for protected loop function
