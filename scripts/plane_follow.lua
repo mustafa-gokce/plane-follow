9d120f904431eb1ae04c0ade0417e23c6ca791e5
m <mustafagokce009@gmail.com>
2022-05-13 15:21:41 +0300

    get airspeed setpoint from vehicle

--- scripts/plane_follow.lua
+++ scripts/plane_follow.lua
@@ -2,12 +2,17 @@
 local LOOP_UPDATE_RATE_HZ = 20 -- main loop update interval
 
 -- global variable declarations
-local target_pos = Location()
-local reference_pos = Location()
-local current_pos = Location()
-local target_velocity = Vector3f()
-local target_heading = 0.0
-local have_target = false
+local follow_target_exist = false
+local follow_target_position = Location()
+local follow_target_velocity = Vector3f()
+local follow_target_heading = 0.0
+local vehicle_reference_position = Location()
+local vehicle_current_position = Location()
+local vehicle_setpoint_airspeed = 0.0
+local vehicle_current_airspeed = 0.0
+local vehicle_target_airspeed = 0.0
+
+-- global enumerations and definitions
 local ENUM_MODE_GUIDED = 15
 
 function check_parameters() -- check key parameters
@@ -30,25 +35,25 @@ end
 
 function update_target() -- update target state
     if not follow:have_target() then
-        if have_target then
+        if follow_target_exist then
             gcs:send_text(0, "lost leader")
         end
-        have_target = false -- vehicle does not have a leader
+        follow_target_exist = false -- vehicle does not have a leader
         return
     end
-    if not have_target then
+    if not follow_target_exist then
         gcs:send_text(0, "found leader")
     end
-    have_target = true -- vehicle has a leader
+    follow_target_exist = true -- vehicle has a leader
 
-    target_pos, target_velocity = follow:get_target_location_and_velocity_ofs() -- get location and velocity of the leader
-    target_pos:change_alt_frame(0) -- altitude frame type of the target position should be absolute
-    target_heading = follow:get_target_heading_deg() -- -- get heading of the leader
+    follow_target_position, follow_target_velocity = follow:get_target_location_and_velocity_ofs() -- get location and velocity of the leader
+    follow_target_position:change_alt_frame(0) -- altitude frame type of the target position should be absolute
+    follow_target_heading = follow:get_target_heading_deg() -- -- get heading of the leader
 end
 
 function update() -- main function that will be called within loop
-    current_pos = ahrs:get_position() -- update current position
-    if not current_pos then
+    vehicle_current_position = ahrs:get_position() -- update current position
+    if not vehicle_current_position then
         return -- do not proceed if vehicle does not have position yet
     end
 
@@ -56,16 +61,19 @@ function update() -- main function that will be called within loop
         return -- do not proceed if vehicle is not in GUIDED mode
     end
 
+    vehicle_current_airspeed = ahrs:airspeed_estimate() -- get current airspeed of the vehicle
+    vehicle_setpoint_airspeed = vehicle:get_target_airspeed() -- get airspeed setpoint of the vehicle
+
     update_target() -- update target data
-    if not have_target then
+    if not follow_target_exist then
         return -- do not proceed if vehicle does not have a target yet
     end
 
-    reference_pos = target_pos:copy() -- copy target position to reference position
-    reference_pos:offset_bearing(target_heading + 180, 1000) -- calculate reference position
-    reference_pos:change_alt_frame(0) -- change altitude frame of the reference position to absolute
+    vehicle_reference_position = follow_target_position:copy() -- copy target position to reference position
+    vehicle_reference_position:offset_bearing(follow_target_heading + 180, 1000) -- calculate reference position
+    vehicle_reference_position:change_alt_frame(0) -- change altitude frame of the reference position to absolute
 
-    vehicle:update_target_location(reference_pos, target_pos) -- update target location of the vehicle
+    vehicle:update_target_location(vehicle_reference_position, follow_target_position) -- update target location of the vehicle
 end
 
 function loop() -- loop function to call main update function
